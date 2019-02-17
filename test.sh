00f82a43e0cbf69619560c44268ad2a7c36d422e
prathikshaa-r <pratsr996@gmail.com>
2019-02-17 16:26:26 -0500

    tests updated with player

--- test.sh
+++ test.sh
@@ -3,39 +3,87 @@
 make
 
 echo "------------------------------------------------"
-echo "Test 0\n./ringmaster 4444 10 10"
+echo "------------------------------------------------"
+echo "Ringmaster Tests"
+
+echo "------------------------------------------------"
+echo "Test 0:\tVALID\n./ringmaster 4444 10 10"
 echo "------------------------------------------------"
 ./ringmaster 4444 10 10
 
 echo "------------------------------------------------"
-echo "Test 1\n./ringmaster 4444 1 10"
+echo "Test 1:\tVALID\n./ringmaster 4444 1 10"
 echo "------------------------------------------------"
 ./ringmaster 4444 1 10
 
 echo "------------------------------------------------"
-echo "Test 2\n./ringmaster qrty wertyu dfgh rty"
+echo "Test 2:\tINVALID\n./ringmaster qrty wertyu dfgh rty"
 echo "------------------------------------------------"
 ./ringmaster qrty wertyu dfgh rty # 4+1
 
 echo "------------------------------------------------"
-echo "Test 3\n./ringmaster 122 1322"
+echo "Test 3:\tINVALID\n./ringmaster 122 1322"
 echo "------------------------------------------------"
 ./ringmaster 122 1322 # 4-1
 
 echo "------------------------------------------------"
-echo "Test 4\n./ringmaster qwe qwe qew"
+echo "Test 4:\tINVALID\n./ringmaster qwe qwe qew"
 echo "------------------------------------------------"
 ./ringmaster qwe qwe qew # correct # -- incorrect data format
 
 echo "------------------------------------------------"
-echo "Test 5\n./ringmaster -123 -124 -132"
+echo "Test 5:\tINVALID\n./ringmaster -123 -124 -132"
 echo "------------------------------------------------"
 ./ringmaster -123 -124 -132 # -ve numbers
 
 echo "------------------------------------------------"
-echo "Test 6\n./ringmaster    -123   -124 -132"
+echo "Test 6:\tINVALID\n./ringmaster    -123   -124 -132"
 echo "------------------------------------------------"
 ./ringmaster    -123   -124 -132 # -ve numbers
+
+echo "------------------------------------------------"
+echo "------------------------------------------------"
+echo "Player Tests"
+
+echo "------------------------------------------------"
+echo "Test 0:\tVALID\n./player 127.0.0.1 4444"
+echo "------------------------------------------------"
+./player 127.0.0.1 4444
+
+echo "------------------------------------------------"
+echo "Test 1:\tVALID\n./player vcm-6252.vm.duke.edu 4444"
+echo "------------------------------------------------"
+./player vcm-6252.vm.duke.edu 4444
+
+echo "------------------------------------------------"
+echo "Test 2:\tVALID\n./player localhost 4444"
+echo "------------------------------------------------"
+./player localhost 4444
+
+echo "------------------------------------------------"
+echo "Test 3:\tINVALID\n./player qrty wertyu dfgh"
+echo "------------------------------------------------"
+./player qrty wertyu dfgh # 3+1
+
+echo "------------------------------------------------"
+echo "Test 4:\tINVALID\n./player 122"
+echo "------------------------------------------------"
+./player 122 # 3-1
+
+echo "------------------------------------------------"
+echo "Test 5:\tINVALID\n./player qwe qwe"
+echo "------------------------------------------------"
+./player qwe qwe # correct # -- incorrect data format
+
+echo "------------------------------------------------"
+echo "Test 6:\tINVALID\n./player -123 -124"
+echo "------------------------------------------------"
+./player -123 -124 # -ve numbers
+
+echo "------------------------------------------------"
+echo "Test 7:\tINVALID\n./player    -123   -124"
+echo "------------------------------------------------"
+./player    -123   -124 # -ve numbers
 
 echo "------------------------------------------------"
 make clean
