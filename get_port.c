911fecdf3ff94bda4f2cfe24e52c6528794244f7
prathikshaa-r <pratsr996@gmail.com>
2019-02-24 00:50:40 -0500

    testing program -- player send listener host and port

---
+++ get_port.c
@@ -0,0 +1,49 @@
+#include "potato.h"
+
+int main() {
+  int fd = open_server_socket(NULL, "0");
+
+  if (fd == -1) {
+    fprintf(stderr, "failed to start server on port 4444\n");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in listen_addr;
+  socklen_t listen_addr_len = sizeof(listen_addr);
+
+  int sockname_status =
+      getsockname(fd, (struct sockaddr *)&listen_addr, &listen_addr_len);
+
+  if (sockname_status == -1) {
+    perror("getsockname:");
+    exit(EXIT_FAILURE);
+  }
+
+  size_t len = SHORT_MSG_SIZE;
+  char my_hostname[len]; // = NULL;
+  if (gethostname(my_hostname, len) != 0) {
+    perror("Failed to get host name.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (my_hostname[len - 1] != '\0') {
+    printf("hostname longer than %lu characters:%s\n", len, my_hostname);
+  }
+  printf("hostname:\t%s\n", my_hostname);
+  int my_port = ntohs(listen_addr.sin_port);
+
+  printf("Listening on port %d\n", my_port);
+
+  char buf_my_serv_info[SHORT_MSG_SIZE];
+  len = SHORT_MSG_SIZE;
+  memset(&buf_my_serv_info, 0, len);
+  if (snprintf(buf_my_serv_info, len, "hostname~%s|port~%d", my_hostname,
+               my_port) < 0) {
+    fprintf(stderr, "building string using snprintf failed.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("%s\n", buf_my_serv_info);
+
+  return EXIT_SUCCESS;
+}
