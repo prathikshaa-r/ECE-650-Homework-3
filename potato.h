74b9e3d6b06ab31387cb28888c08860fa6b77814
prathikshaa-r <pratsr996@gmail.com>
2019-02-23 21:45:55 -0500

    implemented open_server_socket, open_client_socket in potato.h

--- potato.h
+++ potato.h
@@ -11,10 +11,18 @@
 #ifndef __POTATO_H__
 #define __POTATO_H__
 
+#define LISTEN_BACKLOG 100
+
+#include <assert.h>
 #include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 typedef struct _potato_t {
   size_t num_hops;
@@ -41,12 +49,34 @@ typedef struct _player_info_t {
   struct _player_info_t *right;
 } player_info_t;
 
+/*----------------server and client socket------------------*/
+// open a socket connection that listens -- server side
+// implemented in potato.h
+// return socket fd
+
+int open_server_socket(char *hostname, char *port);
+
+// open a socket connection to connnect to specified server
+// implemented in potato.h
+
+int open_client_socket(char *server_hostname, char *server_port);
+/*-----------------------------------------------------------*/
+
+/*-----------------------parsing functions-------------------*/
+// ringmaster inputs
 void parse_rm_input(int margv, char *margc[], ringmaster_inputs_t *inputs);
+
+// player inputs
 void parse_p_inputs(int margv, char *margc[], player_inputs_t *inputs);
+
+// string to number conversion function -- implemented in potato.h
 size_t str_to_num(char *str);
 
+// parse messsages from ringmaster -- implemented in potato.h
 void parse_msgs(char *msg, char *results[], size_t num_fields);
+/*-------------------------------------------------------------*/
 
+/*----------------Function Implementations---------------------*/
 void parse_msgs(char *msg, char *results[], size_t num_fields) {
   // find the fields using pipe -- |
   char *str1, *str2, *field, *value, *n_value;
@@ -80,11 +110,6 @@ void parse_msgs(char *msg, char *results[], size_t num_fields) {
   return;
 }
 
-// open a socket connection that listens -- server side
-int server_socket(char *server_IP_addr,
-                  size_t server_port_num); // return socket fd
-int client_socket(char *server_IP_addr, size_t server_port_num);
-
 // todo: is the parse_input function vulnerable to buffer overflow due to
 // dynamic memory alloc?
 size_t str_to_num(char *str) {
@@ -111,5 +136,109 @@ size_t str_to_num(char *str) {
 
   return val;
 }
+
+int open_server_socket(char *hostname, char *port) {
+  assert(hostname == NULL); // for server socket
+  str_to_num(port);
+
+  int fd;
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *addr_list, *addr_iterator;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  status = getaddrinfo(hostname, port, &hints, &addr_list);
+
+  if (status != 0) {
+    fprintf(stderr, "Error: getaddrinfo\n%s", gai_strerror(status));
+    return -1;
+  }
+
+  for (addr_iterator = addr_list; addr_iterator != NULL;
+       addr_iterator = addr_iterator->ai_next) {
+    fd = socket(addr_iterator->ai_family, addr_iterator->ai_socktype,
+                addr_iterator->ai_protocol);
+    if (fd == -1) {
+      // socket failed at addr. try next addr
+      continue;
+    }
+
+    /*----------socket succeeded-------*/
+    int yes = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("Error:setsockopt\n");
+      return -1;
+    }
+
+    /*---------setsockopt succeeded-------*/
+    // bind
+    if (bind(fd, addr_iterator->ai_addr, addr_iterator->ai_addrlen) == 0) {
+      // bind succeeded -- get out of loop
+      break;
+    }
+
+    /*----------bind failed--------------*/
+    close(fd);
+  }
+
+  if (addr_iterator == NULL) {
+    fprintf(stderr, "Error: failed to bind to any of the addresses retrieved");
+    return -1;
+  }
+
+  // listen
+  if (listen(fd, LISTEN_BACKLOG) == -1) {
+    perror("Error: cannot listen on socket\n");
+    return -1;
+  }
+
+  printf("Listening on port %s\n", port); // remove
+  return fd;
+}
+
+int open_client_socket(char *server_hostname, char *server_port) {
+  int fd;
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *addr_list, *addr_it;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  status = getaddrinfo(server_hostname, server_port, &hints, &addr_list);
+  if (status != 0) {
+    perror("ERROR: getaddrinfo\n");
+    return -1;
+  }
+
+  for (addr_it = addr_list; addr_it != NULL; addr_it = addr_it->ai_next) {
+    fd = socket(addr_it->ai_family, addr_it->ai_socktype, addr_it->ai_protocol);
+    if (fd == -1) {
+      continue;
+    }
+
+    int yes = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("ERROR: sersockopt\n");
+      return -1;
+    }
+
+    if (connect(fd, addr_it->ai_addr, addr_it->ai_addrlen) != -1) {
+      // connect successful
+      break;
+    }
+    // connect failed
+    close(fd);
+  }
+
+  return fd;
+}
+/*---------------------end implementations----------------------*/
 
 #endif
